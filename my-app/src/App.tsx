dac321904e9660655b5945edfc930c978df6e042
Ildar Sahautdinov <scams1992@gmail.com>
2023-10-31 14:37:21 +0500

    reinstall CRA on TS

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t } = useTranslation();
-
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {t('siteName')}
+          Learn React
         </a>
       </header>
     </div>
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,20 +3,19 @@ import 'bootstrap';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 import App from './App';
-import { initI18next } from './initI18next';
+import reportWebVitals from './reportWebVitals';
 
-const app = async () => {
-    await initI18next();
-    
-    const root = ReactDOM.createRoot(document.getElementById('shop') as HTMLElement);
-    root.render(
-        <App />
-    );
-  };
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
